71ddd82c4193d5b5e1361c057564d55734b24328
andre-esc <109835786+hehlow-anon@users.noreply.github.com>
2025-10-24 22:30:02 +0200

    file structure

--- frontend/my-app/src/main.tsx
+++ frontend/my-app/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MainMenu from './MainMenu.tsx'
+import MainMenu from './screens/MainMenu/MainMenu.tsx'
+import Test from './screens/test/test.tsx'
 
 import { BrowserRouter, Routes, Route } from 'react-router'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainMenu />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ frontend/my-app/src/screens/MainMenu/MainMenu.tsx
@@ -0,0 +1,15 @@
+import './MainMenu.css'
+
+function MainMenu() {
+  return (
+    <div id="mainMenu">
+      <p className="title">BARBICHETTE</p>
+      <div className="menuButtons">
+        <button>LMAO</button>
+        <button>LMAO</button>
+      </div>
+    </div>
+  )
+}
+
+export default MainMenu
