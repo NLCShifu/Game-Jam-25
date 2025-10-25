88c3c47b96cb01a00fbd08dbd2e6cef0687ba41c
Romain Croughs <romain.croughs@gmail.com>
2025-10-25 16:21:34 +0200

    feat: images

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.rest.rooms import router as rooms_router
+from api.rest.images import router as images_router
 from api.ws.video import router as video_router
 from api.ws.meta import router as meta_router
 from api.ws.audio import router as audio_router
@@ -14,13 +15,14 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # ou ["*"] pour tester rapidement
+    allow_origins=origins,  # ou ["*"] pour tester rapidement
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 # Les REST endpoints sont montés directement à la racine
 app.include_router(rooms_router)
+app.include_router(images_router)
 
 # Les websockets aussi
 app.include_router(video_router)
